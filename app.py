156cf4c0f7f1d49df3f5da336489783d7b88b9ff
Lucas Moreira <131417982+LucasVPastor@users.noreply.github.com>
2023-12-12 01:05:50 -0300

    wdinw

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
 
 @socketio.on('connect')
 def handle_connect():
     print('Novo jogador conectado!')
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
